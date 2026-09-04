4afce7a1a3e0aa2b9a1e4eb39001dd1d9bb59288
valentine <valentinedinyelu67@gmail.com>
2026-09-04 12:00:46 +0100

    Changes made at backend

--- frontend/src/admin/AdminDashboard.jsx
+++ frontend/src/admin/AdminDashboard.jsx
@@ -1,12 +1,24 @@
 import { useTheme } from "../Context/ThemeContext";
+import AdminSidebar from "./AdminSidebar";
 import { useState, useEffect } from "react";
+
+
 
 const AdminDashboard = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <div>
-            <h2>Admin DashBoard</h2>
+        <div className="flex w-full min-h-screen bg-gray-100 dark:bg-gray-800 text-gray-600 dark:text-gray-100">
+            
+            {/* Admin Sidebar section */}
+            <div>
+                <AdminSidebar />
+            </div>
+
+            {/* Admin Main section*/}
+            <div>
+                <h2>Main Page</h2>
+            </div>
         </div>
     )
 }
---
+++ frontend/src/admin/AdminSidebar.jsx
@@ -0,0 +1,14 @@
+import { User, Upload, Settings, LayoutDashboard, File, FolderKanban, Briefcase, Eye } from "lucide-react";
+import { useState, useEffect } from "react";
+import api from "../constants/Api";
+
+
+const AdminSidebar = () => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default AdminSidebar;
