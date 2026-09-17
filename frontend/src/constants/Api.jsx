90fcee7e6cadde994a0c677cdf080683326f193c
valentine <valentinedinyelu67@gmail.com>
2026-09-17 23:18:16 +0100

    Changes made on frontend api

--- frontend/src/constants/Api.jsx
+++ frontend/src/constants/Api.jsx
@@ -1,12 +1,32 @@
-import axios from "axios"; 
+// import axios from "axios"; 
+
+// const isProduction = import.meta.env.PROD;
+
+// const BASE_URL = "http://localhost:8000/api"; 
+
+// export const BASE_IMAGE_URL = isProduction 
+//     ? import.meta.env.VITE_API_PRODUCTION_URL 
+//     : "http://localhost:8000";
+
+// export const api = axios.create({
+//     baseURL: BASE_URL,
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     withCredentials: true,
+// });
+
+
+import axios from "axios";
 
 const isProduction = import.meta.env.PROD;
 
-const BASE_URL = "http://localhost:8000/api"; 
 
 export const BASE_IMAGE_URL = isProduction 
-    ? import.meta.env.VITE_API_PRODUCTION_URL 
+    ? "https://onrender.com" 
     : "http://localhost:8000";
+
+const BASE_URL = `${BASE_IMAGE_URL}/api`;
 
 export const api = axios.create({
     baseURL: BASE_URL,
@@ -15,5 +35,3 @@ export const api = axios.create({
     },
     withCredentials: true,
 });
-
-// postgresql://admin:7BiifMMoCzEO0hSXUM5Cduxw5EOoDN8A@dpg-dam3i2ncgkoc73877ltg-a/portfolio_eeyu
