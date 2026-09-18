0818521e3b66909f65fa33706a2bfe8dfc9d916a
valentine <valentinedinyelu67@gmail.com>
2026-09-18 11:08:39 +0100

    commit changes

--- frontend/src/constants/Api.jsx
+++ frontend/src/constants/Api.jsx
@@ -1,16 +1,41 @@
+// import axios from "axios";
+
+// const isProduction = import.meta.env.PROD;
+
+// export const BASE_WS_URL = isProduction
+//     ? "wss://://onrender.com"
+//     : "ws://localhost:8000";
+
+// export const BASE_IMAGE_URL = isProduction 
+//     ? "https://portofolio-backend-ggxw.onrender.com" 
+//     : "http://localhost:8000";
+
+// const BASE_URL = `${BASE_IMAGE_URL}/api`;
+
+// export const api = axios.create({
+//     baseURL: BASE_URL,
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     withCredentials: true,
+// });
+
+
 import axios from "axios";
 
-const isProduction = import.meta.env.PROD;
+const isProduction = window.location.hostname.includes("vercel.app") || import.meta.env.PROD;
 
-export const BASE_WS_URL = isProduction
-    ? "wss://://onrender.com"
-    : "ws://localhost:8000";
 
 export const BASE_IMAGE_URL = isProduction 
-    ? "https://portofolio-backend-ggxw.onrender.com" 
+    ? "https://onrender.com" 
     : "http://localhost:8000";
 
 const BASE_URL = `${BASE_IMAGE_URL}/api`;
+
+
+export const BASE_WS_URL = isProduction
+    ? "wss://://onrender.com"  
+    : "ws://localhost:8000";  
 
 export const api = axios.create({
     baseURL: BASE_URL,
