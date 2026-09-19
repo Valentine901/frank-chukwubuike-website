8dc857fb43bd10315a3505db01e2242f1fe2c81e
valentine <valentinedinyelu67@gmail.com>
2026-09-19 12:31:12 +0100

    Initial commit

--- frontend/src/pages/HomePg.jsx
+++ frontend/src/pages/HomePg.jsx
@@ -165,13 +165,13 @@ import ScrollReveal from "../components/ScrollReveal";
 
 const HomePg = () => {
     const { adminProfile, user } = useAuth(); 
-    // if (!adminProfile && !user) {
-    //     return (
-    //         <div className="w-full min-h-screen bg-gray-100 dark:bg-gray-800 flex items-center justify-center">
-    //             <div className="w-8 h-8 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
-    //         </div>
-    //     );
-    // }
+    if (adminProfile || user === null) {
+        return (
+            <div className="w-full min-h-screen bg-gray-100 dark:bg-gray-800 flex items-center justify-center">
+                <div className="w-8 h-8 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+            </div>
+        );
+    }
 
     return (
         <section
