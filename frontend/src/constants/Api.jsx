960e8817a8b6d14d054c7026fc5c8f9f012359fe
valentine <valentinedinyelu67@gmail.com>
2026-09-21 20:22:55 +0100

    first commit

--- frontend/src/constants/Api.jsx
+++ frontend/src/constants/Api.jsx
@@ -7,7 +7,8 @@ export const BASE_WS_URL = isProduction
     ? "wss://portofolio-backend-ggxw.onrender.com"
     : "ws://localhost:8000";
 
-export const BASE_IMAGE_URL = "http://localhost:8000";
+export const BASE_IMAGE_URL = import.meta.env.VITE_BASE_IMAGE_URL || 
+    (isProduction ? "https://portofolio-backend-ggxw.onrender.com" : "http://localhost:8000");
 
 const BASE_URL = `${BASE_IMAGE_URL}/api`;
 
